4e5b0646a7ba348cd404236522ebd9f587b75648
thiagoyb <thiagoyb@gmail.com>
2024-05-21 13:58:03 -0300

    add logo oficiial

--- panel/nav.php
+++ panel/nav.php
@@ -5,7 +5,7 @@
 	<div class="col-12">
 		<div class="col-lg-12 col-sm-12" align="center">
 			<a class="navbar-brand js-scroll-trigger" href="index.php">
-				<img class="logo mb-2 mt-3" width="200" height="auto" src="<?= URL_HOME; ?>assets/img/logo.png" />
+				<img class="logo mb-2 mt-3" width="200" height="auto" src="<?= URL_HOME; ?>assets/img/LogotipoSigaEventos.png" />
 				<span></span>
 			</a>
 
@@ -14,43 +14,43 @@
 			</button>
 		</div>
 
-		<div  class="col-lg-12">			
+		<div  class="col-lg-12">
 		  <div class="navbar-collapse collapse"  id="navbarNavDropdown">
 			<ul class="navbar-nav text-center" style="margin:0 auto;">
 				<li class="nav-item active">
-				  <a class="nav-link" href="index.php" style="color: #fff;">Início</a>
-				</li> 
+				  <a class="nav-link" href="index.php">Início</a>
+				</li>
 
 				<?php
-				  if($user->getPerfil()=='TI'){ ?>
+				if($user->getPerfil()=='TI'){ ?>
 				<li class="nav-item">
-				  <a class="nav-link" href="index.php?p=Menu" style="color: #fff;">Menu Principal</a>
+				  <a class="nav-link" href="index.php?p=Menu">Menu Principal</a>
 				</li><?php
-				  }
+				}
 				?>
 
 				<li class="nav-item">
-				  <a class="nav-link" href="index.php?p=Banners" style="color: #fff;">Banners</a>
+				  <a class="nav-link" href="index.php?p=Banners">Banners</a>
 				</li>
 
 				<!--<li class="nav-item">
-				  <a class="nav-link" href="index.php?p=Arquivos" style="color: #fff;">Arquivos</a>
+				  <a class="nav-link" href="index.php?p=Arquivos">Arquivos</a>
 				</li>
 
 				<li class="nav-item">
-				  <a class="nav-link" href="index.php?p=Paginas" style="color: #fff;">Páginas</a>
+				  <a class="nav-link" href="index.php?p=Paginas">Páginas</a>
 				</li>-->
 
 				<li class="nav-item">
-				  <a class="nav-link" href="index.php?p=Eventos" style="color: #fff;">Eventos</a>
+				  <a class="nav-link" href="index.php?p=Eventos">Eventos</a>
 				</li>
 
 				<li class="nav-item">
-				  <a class="nav-link" href="index.php?p=Noticias" style="color: #fff;">Notícias</a>
+				  <a class="nav-link" href="index.php?p=Noticias">Notícias</a>
 				</li>
 
 				<li class="nav-item dropdown">
-				  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: #fff;">
+				  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 					Conta
 				  </a>
 				  <UL class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
