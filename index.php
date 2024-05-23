64a82053416ccb2b9888fc14aed0e832081b4328
Thiago <2201028@aluno.univesp.br>
2024-05-23 20:28:13 -0300

    separado cont pagina inicial outro arquivo

--- index.php
+++ index.php
@@ -32,26 +32,7 @@ $user = User::auth(__FILE__, true);//permite Visitante
   <?php
 		require_once $page; ?>
 	  </div><?php
-	} else{ ?>
-		<section class="row container">
-			<div class="col-5">
-				<div class="logotipo">
-					<img class="logo" src="src/imagem/LogotipoSigaEventos2.jpg" alt="Logo Siga Eventos" style="    zoom: 0.7;">
-				</div>
-			</div>
-
-			<div class="col-7" id="register-section">
-				<h1>Cadastre seus eventos, aqui!</h1>
-				<h2>"Registre seus eventos de todos os setores: desde iniciativas individuais até ações corporativas, organizações sem fins lucrativos e projetos beneficentes.</h2>
-				<h2>Seja parte desta plataforma e promova suas atividades para um público maior!"</h2>
-			</div>
-		</section>
-		<section class="row container">
-			<div class="col-12 text-center " id="register-button">
-				<a href="cadastro.html" class="button">CADASTRE-SE</a>
-			</div>
-		</section>
-	<?php } ?>
+	} ?>
 	</section>
 	<?php
 	require_once 'footer.php'; ?>
--- inicio.php
+++ inicio.php
@@ -1,52 +1,18 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-	<META http-equiv="X-UA-Compatible" content="IE=edge" />
-    <META http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Siga Eventos</title>
-	<LINK rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"></LINK>
-	<LINK rel="stylesheet" type="text/css" href="assets/css/bootstrap.css"></LINK>
-	<link rel="stylesheet" href="src/css/estiloIndex.css" />
-    <link rel="stylesheet" href="src/css/reset.css" />
-</head>
-<body>
+	<section class="row container">
+			<div class="col-5">
+				<div class="logotipo">
+					<img class="logo" src="src/imagem/LogotipoSigaEventos2.jpg" alt="Logo Siga Eventos" style="    zoom: 0.7;">
+				</div>
+			</div>
 
-<section class="row container">
-	<div class="col-5">
-		<div class="logotipo">
-			<img class="logo" src="src/imagem/LogotipoSigaEventos2.jpg" alt="Logo Siga Eventos" style="    zoom: 0.7;">
-		</div>
-	</div>
-
-    <div class="col-7" id="register-section">
-        <h1>Cadastre seus eventos, aqui!</h1>
-        <h2>"Registre seus eventos de todos os setores: desde iniciativas individuais até ações corporativas, organizações sem fins lucrativos e projetos beneficentes.</h2>
-        <h2>Seja parte desta plataforma e promova suas atividades para um público maior!"</h2>
-	</div>
-</section>
-
-<section class="row container">
-    <div class="text-center " id="register-button">
-        <a href="cadastro.html" class="button">CADASTRE-SE</a>
-    </div>
-</section>
-
-<nav id="menu">
-    <ul class="menu">
-        <li><a class="nav-link" href="index.html">Início</a></li> <!-- Link para a página "index.html" -->
-        <li><a class="nav-link" href="eventos.html">Eventos</a></li> <!-- Link para a página "eventos.html" -->
-        <li><a class="nav-link" href="informações.html">Informações</a></li> <!-- Link para a página "informacoes.html" -->
-        <li><a  class="nav-link"href="login.html">Login</a></li> <!-- Link para a página "login.html" -->
-    </ul>
-</nav>
-
-<!--<div id="main-content">
-    <div id="event-follow-section">
-        <video controls src="./src/imagem/LogotipoSigaEventoslaranjacirculo2.mp4" type="Logotipo Siga Eventos" srcset=""></video>
-    </div>
-</div>-->
-
-	<script src='script.js'></script>
-</body>
-</html>
+			<div class="col-7" id="register-section">
+				<h1>Cadastre seus eventos, aqui!</h1>
+				<h2>"Registre seus eventos de todos os setores: desde iniciativas individuais até ações corporativas, organizações sem fins lucrativos e projetos beneficentes.</h2>
+				<h2>Seja parte desta plataforma e promova suas atividades para um público maior!"</h2>
+			</div>
+		</section>
+		<section class="row container">
+			<div class="col-12 text-center " id="register-button">
+				<a href="cadastro.html" class="button">CADASTRE-SE</a>
+			</div>
+		</section>
